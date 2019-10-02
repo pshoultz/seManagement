d2c1a3fa3b6516898a9714450e76c59d0d08ca32
pshoultz <pshoultzula@gmail.com>
2019-10-02 09:40:13 -0500

    updates

--- api.js
+++ api.js
@@ -5,8 +5,16 @@ const PORT = 8080;
 const HOST = "0.0.0.0";
 
 
-app.get('/', function (req, res) {
-  res.send('/ route')
+app.get('/version', function (req, res) {
+    res.send('this is verison 0 of the hotBurger service' )
+})
+
+app.get('/logs', function (req, res) {
+    var log = {
+
+    };
+
+    res.send(log);
 })
 
 app.listen(PORT, HOST);
