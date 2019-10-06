a0205e82c94d85f44a03f3a78cb8b2f94e7fdaaf
pshoultz <pshoultzula@gmail.com>
2019-10-06 12:05:09 -0500

    done with project

--- api.js
+++ api.js
@@ -6,29 +6,26 @@ const PORT = 8080;
 const HOST = "0.0.0.0";
 
 //NOTE : logger for telemetry
-app.use('/', function (req, res, next){
-    var log = req.method + "," + req.originalUrl + "," + Date.now() + "\n"
+app.use(function (req, res, next){
+    var log = req.method + "," + req.originalUrl + "," + res.statusCode + "\n";
+    console.log(res.status);
     fs.appendFile('logger.txt', log, (err) => {
         if(err) throw err
-        console.log("log saved");
     });
     next();
 });
-
-app.get('/', function (req, res) {
-    res.send('default home route' )
-})
 
 app.get('/version', function (req, res) {
     res.send('this is verison 0 of the hotBurger service' )
 })
 
 app.get('/logs', function (req, res) {
-    var log = {
-
-    };
-
-    res.send(log);
+    var contents = fs.readFileSync("./logger.txt", 'utf8');
+    console.log(contents);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(contents);
 })
+
+
 
 app.listen(PORT, HOST);
---
+++ readme.txt
@@ -0,0 +1,1 @@
+http://ec2-13-58-124-197.us-east-2.compute.amazonaws.com/
