7683b8015ab3ab88b37cab1975d053cde8ec03b0
pshoultz <pshoultzula@gmail.com>
2019-10-06 08:42:33 -0500

    updates for project

--- api.js
+++ api.js
@@ -1,8 +1,19 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
 const PORT = 8080;
 const HOST = "0.0.0.0";
+
+//NOTE : logger for telemetry
+app.use('/', function (req, res, next){
+    var log = req.method + "," + req.originalUrl + "," + Date.now() + "\n"
+    fs.appendFile('logger.txt', log, (err) => {
+        if(err) throw err
+        console.log("log saved");
+    });
+    next();
+});
 
 app.get('/', function (req, res) {
     res.send('default home route' )
---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker container run -d -p 80:8080 --name con-sem img-sem:0.1
