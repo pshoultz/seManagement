d19ad6a5a4d4b87d502915c50f3f35ad6f8ac6cb
pshoultz <pshoultzula@gmail.com>
2019-11-03 15:38:58 -0600

    just need to do the other service, the volume, and then the docker-compose.yml

--- api.js
+++ api.js
@@ -5,28 +5,29 @@ var fs = require('fs');
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
-//NOTE : logger for telemetry
-app.use(function (req, res, next){
-    var log = req.method + "," + req.originalUrl + "," + res.statusCode + "\n";
-    console.log(res.status);
-    fs.appendFile('logger.txt', log, (err) => {
-        if(err) throw err
+app.get('/version', function (req, res) {
+    res.send('this is verison 1 of the hotBurger service' )
+})
+
+//app.get('/logs', function (req, res) {
+//    var contents = fs.readFileSync("./logger.txt", 'utf8');
+//    console.log(contents);
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.write(contents);
+//    //res.end();
+//})
+
+app.get('/getMenu', function(req,res){
+    res.send({
+        "hotdong":20,
+        "hamburger":35,
+        "soda":4,
+        "cookie":6
     });
-    next();
 });
 
-app.get('/version', function (req, res) {
-    res.send('this is verison 0 of the hotBurger service' )
-})
-
-app.get('/logs', function (req, res) {
-    var contents = fs.readFileSync("./logger.txt", 'utf8');
-    console.log(contents);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(contents);
-    //res.end();
-})
-
-
+app.post('/purchase/:item/:quantity', function(req,res){
+    console.log(req.params.item, req.params.quantity);
+});
 
 app.listen(PORT, HOST);
