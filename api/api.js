11cb4a14cc8224a7a3115a78d05661f3221e9ba9
pshoultz <pshoultzula@gmail.com>
2019-11-24 12:11:05 -0600

    application complete minus docker stuff

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var http = require('http');
 
 const PORT = 8080;
 const HOST = "0.0.0.0";
@@ -19,13 +20,13 @@ app.get('/version', function (req, res) {
     res.send('this is verison 1 of the hotBurger service' )
 })
 
-app.get('/logs', function (req, res) {
-    var contents = fs.readFileSync("./data.txt", 'utf8');
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(contents);
-    console.log(contents);
-    res.end();
-})
+//app.get('/logs', function (req, res) {
+//    var contents = fs.readFileSync("./data.txt", 'utf8');
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.write(contents);
+//    console.log(contents);
+//    res.end();
+//})
 
 app.get('/getMenu', function(req,res){
     res.send({
@@ -37,7 +38,33 @@ app.get('/getMenu', function(req,res){
 });
 
 app.post('/purchase/:item/:quantity', function(req,res){
-    console.log(req.params.item, req.params.quantity);
+    var item = req.params.item;
+    var quantity = req.params.quantity;
+    console.log(req.params);
+    var options = {
+        hostname:"127.0.0.1",
+        port:8081,
+        path:"/setcount/" + item + "/" + quantity,
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    var req = http.request(options, function(res){
+        console.log("making http request...");
+        res.on('data', (d) => {
+            console.log("request made...");
+        });
+    });
+
+    req.on('error', function(e){
+        console.log(e.message);
+    });
+    req.end();
+
+    res.send(200);
 });
 
+console.log("api.js running... on port " + PORT);
 app.listen(PORT, HOST);
