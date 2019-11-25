8f7c935e0f38698dd9fec85a5a157599bfc6ee9b
pshoultz <pshoultzula@gmail.com>
2019-11-25 17:47:51 -0600

    nginx causing the issue?

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ cd ./api && docker image build -t img-sem-api . --no-cache &&
 cd ../monitoring && docker image build -t img-sem-monitoring . --no-cache  &&
 cd ../testharness/user && docker image build -t img-sem-fakeuser . --no-cache &&
 cd ../inventory && docker image build -t img-sem-fakeinventory . --no-cache &&
-cd ../../ && docker-compose up -d
+cd ../../ && docker-compose up
